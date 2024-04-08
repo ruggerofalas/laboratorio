abd21c42891d8bc271ce9000633ce895568722f1
ruggerofalas <159021254+ruggerofalas@users.noreply.github.com>
2024-04-08 12:29:28 +0200

    chat

---
+++ MainChatClient.java
@@ -0,0 +1,9 @@
+package la_chat;
+
+
+public class MainChatClient {
+   public static void main(String args[])
+	{
+		ChatClient chat = new ChatClient();
+	}  
+}
---
+++ MainChatServer.java
@@ -0,0 +1,10 @@
+package la_chat;
+
+
+
+public class MainChatServer {
+    public static void main(String args[])
+	{
+		ChatServer chat = new ChatServer();
+	}
+}
